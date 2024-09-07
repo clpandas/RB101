63a7fd67f17c7e0c0f381bc0cb75236387b5e313
Crystal Lor <114378804+clpandas@users.noreply.github.com>
2024-09-07 06:10:40 +0000

    Ignore the last comment: My last interpretation was false because while variable shadowing happens in teh car method, it did not affect the final results of this program because the shadowed variables were only used within the method. The real error occured previously because I attempted to use the p method during the multiple assignment.

--- RB101/lesson-2/debugging.rb
+++ RB101/lesson-2/debugging.rb
@@ -1,16 +1,3 @@
-=begin
-Another error: 
-ruby debugging.rb
-debugging.rb:7:in `make': wrong number of arguments (given 0, expected 1) (ArgumentError)
-from debugging.rb:15:in `<main>'
-
-This error is because of a name conflict 
-between the local variable `make` inside the `car` method
-and the method `make` itself. 
-Variable shadowing is causing `make` and `model`
-to shadow the method names, leading to an `ArgumentError`
-when trying to call `make` without any arguments.
-=end
 def car(new_car)
   make = make(new_car)
   model = model(new_car)
@@ -25,6 +12,6 @@ def model(new_car)
   new_car.split(" ")[1]
 end
 
-p make, model = car("Ford Mustang")
+make, model = car("Ford Mustang")
 p make == "Ford"
-p mode.start_with?("M")
+p model.start_with?("M")
