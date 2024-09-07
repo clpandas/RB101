6b289d89158217d2d7d7c30e22e70d2f7026810a
Crystal Lor <114378804+clpandas@users.noreply.github.com>
2024-09-07 13:33:40 +0000

    Further exploration: exploring defaults and getting user input.

--- RB101/small-problems/easy-2/how_old_is_teddy.rb
+++ RB101/small-problems/easy-2/how_old_is_teddy.rb
@@ -1,1 +1,14 @@
-puts "Teddy is #{rand(20..200)} years old!"
+def greet(name = 'Teddy')
+  puts "Please enter a name:"
+  user_input = gets.chomp
+  name = user_input unless user_input.empty?
+  age = rand(20..200)
+  puts "#{name} is #{age} years old!"
+end
+
+greet
+
+# # another version
+# puts "Please give me a name:"
+# name = gets.chomp
+# puts "#{name.empty? ? 'Teddy' : name} is #{rand(20..200)} years old!"
