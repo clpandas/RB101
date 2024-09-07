af263e0a70cb89baeb4ed5eee371841132cf589a
Crystal Lor <114378804+clpandas@users.noreply.github.com>
2024-09-07 05:52:45 +0000

    Another error: Variable shadowing is causing an ArgumentError.

--- RB101/lesson-2/debugging.rb
+++ RB101/lesson-2/debugging.rb
@@ -1,3 +1,16 @@
+=begin
+Another error: 
+ruby debugging.rb
+debugging.rb:7:in `make': wrong number of arguments (given 0, expected 1) (ArgumentError)
+from debugging.rb:15:in `<main>'
+
+This error is because of a name conflict 
+between the local variable `make` inside the `car` method
+and the method `make` itself. 
+Variable shadowing is causing `make` and `model`
+to shadow the method names, leading to an `ArgumentError`
+when trying to call `make` without any arguments.
+=end
 def car(new_car)
   make = make(new_car)
   model = model(new_car)
@@ -9,7 +22,7 @@ def make(new_car)
 end
 
 def model(new_car)
-  new_car.split(" ")[2] # this will return nil because there is no element at index 2
+  new_car.split(" ")[1]
 end
 
 p make, model = car("Ford Mustang")
