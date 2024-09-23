5d2732adbef4226dff8245f396d2fc5ece35d18d
Crystal Lor <114378804+clpandas@users.noreply.github.com>
2024-09-23 01:34:29 +0000

    Add #each_with_object and #reduce options for this method.

--- RB101/small-problems/easy-4/running_totals.rb
+++ RB101/small-problems/easy-4/running_totals.rb
@@ -1,14 +1,14 @@
-def running_total(array)
-  total = []
-  sum = 0
+# def running_total(array)
+#   total = []
+#   sum = 0
   
-  array.each do |num|
-    sum += num
-    total << sum
-  end
+#   array.each do |num|
+#     sum += num
+#     total << sum
+#   end
 
-  total
-end
+#   total
+# end
 
 # LS Solution
 # def running_total(array)
@@ -16,9 +16,20 @@ end
 #   array.map { |value| sum += value }
 # end
 
-# Enumerable#each_with_object solution
+# # Enumerable#each_with_object solution
+# def running_total(array)
+#   sum = 0
+#   array.each_with_object([]) { |num, obj| obj << sum += num }
+# end
 
-# Enumerable#reduce solution 
+# Enumerable#reduce solution
+def running_total(array)
+  sum = 0
+  array.reduce([]) do |result, num|
+    sum += num
+    result << sum
+  end
+end 
 
 p running_total([2, 5, 13]) == [2, 7, 20]
 p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
