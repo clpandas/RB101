445b8b4e43d787e5c50c3c19986b0dd8e37da601
Crystal Lor <114378804+clpandas@users.noreply.github.com>
2024-09-27 01:31:06 +0000

    Add further explorations.

--- RB101/small-problems/easy-1/odd.rb
+++ RB101/small-problems/easy-1/odd.rb
@@ -1,10 +1,20 @@
-def is_odd?(number)
-  number % 2 != 0
-end
+# def is_odd?(number)
+#   number % 2 != 0
+# end
 
+# # LS solution
 # def is_odd?(number)
 #   number % 2 == 1
 # end
+
+# # Further exploration
+# def is_odd?(number)
+#   number.abs.remainder(2) != 0
+# end
+
+def is_odd?(number)
+  number.abs.remainder(2) == 1
+end
 
 puts is_odd?(2)
 puts is_odd?(5)
