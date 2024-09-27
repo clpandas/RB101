72fc98493c7b73d304bdd5a09a758198387271aa
Crystal Lor <114378804+clpandas@users.noreply.github.com>
2024-09-27 02:39:07 +0000

    Add further explorations.

--- RB101/small-problems/easy-1/stringy_strings.rb
+++ RB101/small-problems/easy-1/stringy_strings.rb
@@ -1,8 +1,19 @@
-def stringy(num)
+# def stringy(num)
+#   result_string = ''
+#   num.times do |i|
+#     result_string += (i.even? ? '1' : '0')
+#   end
+#   result_string
+# end
+
+# Further Exploration
+def stringy(number, start = 1)
   result_string = ''
-  num.times do |i|
-    result_string += (i.even? ? '1' : '0')
+
+  number.times do |i|
+    result_string << (i.even? ? (start == 1 ? '1' : '0') : (start == 1 ? '0' : '1'))
   end
+  
   result_string
 end
 
@@ -10,3 +21,9 @@ puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+
+# Further Exploration
+puts stringy(6, 0) == '010101'
+puts stringy(9, 0) == '010101010'
+puts stringy(4, 0) == '0101'
+puts stringy(7, 0) == '0101010'
